3ee0015044fb7646b436a07181e6658126a4ce27
Kishor Shrestha <winneecreztha@gmail.com>
2017-12-10 11:30:39 +0545

    second question solved

--- secondsoln.py
+++ secondsoln.py
@@ -1,2 +1,16 @@
 #author : Kishor Shrestha
 #Define a global static password value by yourself. Then define a function which prompts user for a password. If the password is a match, print "password match" or else print "Wrong password"
+
+mainPassword = 'password'
+
+def askPassword():
+    inputPassword = input("Enter the password !!!")
+    if inputPassword == mainPassword:
+        return True
+    else:
+        return False
+
+if askPassword():
+    print("Password matches !")
+else:
+    print("Password Doesnt mathches !!")
