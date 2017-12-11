52808cc37921c405d6b19bbd389b54dd12ddceca
Kishor Shrestha <winneecreztha@gmail.com>
2017-12-11 16:13:56 +0545

    third question solved

--- thirdSoln.py
+++ thirdSoln.py
@@ -1,2 +1,5 @@
-#We have v=u+at where v is velocity, u is initial velocity, and t is time. And value for each is as follows: v=25m/s. u=0m/s, t=10 seconds. Find the acceleration a.
+# We have v=u+at where v is velocity, u is initial velocity, and t is time. And value for each is as follows: v=25m/s. u=0m/s, t=10 seconds. Find the acceleration a.
 
+def acceleration(v=0, u=0, t=0):
+    return (v - u) / t
+print("The acceleration for v = {} , u = {} ,t={} is a = {}m/s".format('25m/s','0m/s','10second',acceleration(25,0,10)))
